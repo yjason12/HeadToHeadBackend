a21524e02e0b7e0b9fbb2ed6482d77716b8979e3
Michael Chen <mdchenmail@gmail.com>
2022-01-11 14:43:38 -0800

    room handling

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const io = require("socket.io")(http, {
 
 var cors = require('cors')
 
+
+//Room name to ARRAY of playerIDs
+var rooms = {};
+
+//IDs to name
+var idToName = {};
+
+//ID to room
+var idToRoom = {};
+
 app.use(cors())
 app.get('/', function(req, res) {
    res.send("pong");
@@ -17,10 +27,20 @@ app.get('/', function(req, res) {
 //Whenever someone connects this gets executed
 io.on('connection', function(socket) {
    console.log('A user connected');
-
+   
    //Whenever someone disconnects this piece of code executed
    socket.on('disconnect', function () {
       console.log('A user disconnected');
+      let roomOfID = idToRoom[socket.id];
+      //console.log('room to modify:' + roomOfID);
+      rooms[roomOfID].splice(rooms[roomOfID].indexOf(socket.id), 1);
+      //console.log("length is " + rooms[roomOfID].length)
+      if(!rooms[roomOfID].length){
+         delete rooms[roomOfID];
+      }
+
+      delete idToName[socket.id];
+      delete idToRoom[socket.id];
    });
 
    /**
@@ -36,7 +56,23 @@ io.on('connection', function(socket) {
 
     socket.on('test', (roomInfo) => {
       console.log('michael connect to yongjae');
+
+      if(socket.id in idToName){
+         io.emit('test', 'already in a room');
+      } else if (roomInfo['room'] in rooms) {
+         rooms[roomInfo['room']].push(socket.id);
+      } else {
+         rooms[roomInfo['room']] = [socket.id];
+      }
+      idToName[socket.id] = roomInfo['id'];
+      idToRoom[socket.id] = roomInfo['room'];
+
+
       io.emit('test', "!we did it!");
+      //console.log(rooms[roomInfo['room']]);
+      io.emit('test', rooms[roomInfo['room']]);
+      io.emit('test', idToName[socket.id]);
+      io.emit('test', idToRoom[socket.id]);
     });
 
 });
