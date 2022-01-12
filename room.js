5804225b8ab2eb8a4625988761055d1803be0478
Jason Lee <jasonyl@Jasons-MacBook-Air.local>
2022-01-11 20:14:38 -0800

    added playerExists method and saftey check to removePlayer

--- room.js
+++ room.js
@@ -9,9 +9,17 @@ class Room {
     }
     
     removePlayer(player){
-        this.players.splice(this.players.map(x => x.playerID).indexOf(player.playerID), 1);
+        if(this.playerExists(player)){
+            this.players.splice(this.players.map(x => x.playerID).indexOf(player.playerID), 1);
+        } else {
+            console.log("tried to remove a player that does not exist in room");
+        }
     }
     
+    playerExists(player){
+        return this.players.map(x => x.playerID).indexOf(player.playerID) != -1;
+    }
+
     getRoomSize(){
         return this.players.length;
     }
